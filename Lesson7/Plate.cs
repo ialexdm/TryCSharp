a8fc30e3d09e27ab1c811dd496994f35eea7618e
Александр <ivashininad@gmail.com>
2022-02-07 22:37:05 +0300

    Complete Lesson 7

---
+++ Lesson7/Plate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    public class Plate
+    {
+        private int _food;
+        public Plate(int food)
+        {
+            _food = food;
+        }
+        public bool DecreaseFood(int n)
+        {
+            bool isDecreased = false;
+            if (_food >= n)
+            {
+                _food -= n;
+                isDecreased = true;
+            }
+            return isDecreased;
+        }
+        public void Fill(int food)
+        {
+            _food += food;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("plate: " + _food);
+        }
+    }
+}
