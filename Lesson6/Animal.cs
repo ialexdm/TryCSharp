cca759dbd020b99c022477d76e1e097ed5ccebc3
Александр <ivashininad@gmail.com>
2022-02-07 17:14:18 +0300

    Complete lesson 6

---
+++ Lesson6/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    public class Animal
+    {
+        protected double _maxSwimDistance;
+        protected double _maxRunDistance;
+        protected double _maxJumpHeight;
+        private static int _count;
+        public static int Count
+        {
+            get
+            { return _count; }
+            set
+            { _count = value; }
+        }
+
+        protected Animal()
+        {
+            Count++;
+        }
+
+        public virtual void Swim(double distance)
+        {
+            bool isSuccess = _maxSwimDistance > distance;
+            Console.WriteLine($"swim: {isSuccess}");
+        }
+        public virtual void Run(double distance)
+        {
+            bool isSuccess = _maxRunDistance > distance;
+            Console.WriteLine($"run: {isSuccess}");
+        }
+        public virtual void Jump(double height)
+        {
+            bool isSuccess = _maxJumpHeight > height;
+            Console.WriteLine($"jump: {isSuccess}");
+        }
+
+    }
+}
---
+++ Lesson6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+/*        1. Создать классы Собака и Кот с наследованием от класса Животное.
+2. Животные могут выполнять действия: бежать, плыть, перепрыгивать препятствие. 
+В качестве параметра каждому методу передается величина, 
+означающая или длину препятствия (для бега и плавания), или высоту(для прыжков).
+3. У каждого животного есть ограничения на действия(бег: кот 200 м., собака 500 м.; прыжок: кот 2 м., собака 0.5 м.; плавание: кот не умеет плавать, собака 10 м.).
+4. При попытке животного выполнить одно из этих действий, оно должно сообщить результат в консоль. (Например, dog1.run(150); -> результат: run: true)
+5. * Добавить подсчет созданных котов, собак и животных.*/
+
+        static void Main(string[] args)
+        {
+            Cat barsik = new Cat();
+            Console.WriteLine($"Animal count:{Animal.Count}, Cat count{Cat.Count}, Dog Count: {Dog.Count}");
+            Cat murka = new Cat();
+            Console.WriteLine($"Animal count:{Animal.Count}, Cat count{Cat.Count}, Dog Count: {Dog.Count}");
+            Dog polkan = new Dog();
+            Console.WriteLine($"Animal count:{Animal.Count}, Cat count{Cat.Count}, Dog Count: {Dog.Count}");
+            polkan.Run(400);
+            barsik.Swim(200);
+        }
+    }
+}
